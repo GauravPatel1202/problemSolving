2af1dd72415594dca285ac11c041bfccfd31cb40
Gaurav Patel <DeepakDevulapalli@LiFung.com>
2019-03-14 11:12:54 +0530

    leftRotatebyOne

--- arrayRotation.js
+++ arrayRotation.js
@@ -1,12 +1,24 @@
-function rotation(arr,){
+function rotation(arr,d,callback){
+ 
  for(i=0;i<d;i++){
-
+    callback(arr)
  }
+ console.log(arr)
 
 }
-function rotationleft(){
-
+function rightRotatebyOne(arr){
+    var temp = arr[arr.length-1], i; 
+    for (i = arr.length - 1; i < 0; i--) 
+        arr[i] = arr[i -1]; 
+  
+    arr[i] = temp; 
+ 
 }
-function rotationRight(){
-
+function leftRotatebyOne(arr){
+    var temp = arr[0], i; 
+    for (i = 0; i < arr.length - 1; i++) 
+        arr[i] = arr[i + 1]; 
+  
+    arr[i] = temp; 
 }
+rotation([1,2,3,4,5],4,leftRotatebyOne)
