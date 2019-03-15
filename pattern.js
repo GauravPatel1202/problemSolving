4c960a48ac22c4599187efc070e1ecf86f7ce985
Gaurav Patel <DeepakDevulapalli@LiFung.com>
2019-03-15 17:11:38 +0530

    pattern

---
+++ pattern.js
@@ -0,0 +1,13 @@
+for(var i=0;i<=5;i++){
+    for(var j=0;j<=i;j++){
+        console.log(i)
+    }
+    console.log("--")
+}
+console.log("-----------------------------------")
+for(var i=0;i<=5;i++){
+    for(var j=4;j>=i;j--){
+        console.log(i)
+    }
+    console.log("----")
+}
